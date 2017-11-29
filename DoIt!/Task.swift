93091896645214597e96dd66aa6ec39a49918faa
David Hoogerheide <jandavidhoogerheide@gmail.com>
2017-11-29 19:57:01 +0100

    no message

---
+++ DoIt!/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt!
+//
+//  Created by David Hoogerheide on 29/11/2017.
+//  Copyright © 2017 David Hoogerheide. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
