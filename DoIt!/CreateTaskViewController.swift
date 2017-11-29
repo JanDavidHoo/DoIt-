041f7dbdbfbe6d410f08122b485874e235ef7d28
David Hoogerheide <jandavidhoogerheide@gmail.com>
2017-11-29 22:33:53 +0100

    letsogo

---
+++ DoIt!/CreateTaskViewController.swift
@@ -0,0 +1,25 @@
+//
+//  CreateTaskViewController.swift
+//  DoIt!
+//
+//  Created by David Hoogerheide on 29/11/2017.
+//  Copyright © 2017 David Hoogerheide. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet weak var importantSwitch: UISwitch!
+    @IBOutlet weak var taskNameTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func addTaskBut(_ sender: Any) {
+    }
+    
+   
+}
